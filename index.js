7c0a286619ccb50418d3a4ed1af0c39decacedc7
Tulasi Ram Reddy <tulasir287@gmail.com>
2022-06-06 19:36:01 +0530

    prepare for host

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.post('/', (req, res, next) => {
     // console.log(req.body);
     // console.log(req.headers);
     console.log(req.body);
+    console.log(req.get('mode'));
+    console.log(req.get('Content-Type'));
     res.set('Access-Control-Allow-Origin', '*');
     res.status(200).json({ status: 'success' });
 });
